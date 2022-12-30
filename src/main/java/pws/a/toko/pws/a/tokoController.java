77c72a06728c854b4e3022660bbbfd2dab4618b2
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-12-30 15:22:15 +0700

    bisa delete

---
+++ src/main/java/pws/a/toko/pws/a/tokoController.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pws.a.toko.pws.a;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ *
+ * @author LENOVO
+ */
+@Controller
+@ResponseBody
+public class tokoController {
+    
+     Barang data = new Barang();
+    BarangJpaController actrl = new BarangJpaController();
+    
+    @RequestMapping("/getAll")
+    public String getAll(int id){
+        data = actrl.findBarang(id);
+        return data.getNama();
+    }
+    
+    
+    @RequestMapping("/getName/{id}")
+    public String getName(@PathVariable("id") int id){
+        try {
+                data = actrl.findBarang(id);
+                return data.getNama()+"<br>"+data.getJumlah();
+            }
+        
+        catch (Exception error){return "Data Tidak Ada";}
+    
+    }
+    
+    @RequestMapping("/delete/{id}")
+    public String deleteData(@PathVariable("id") int id){
+        try{
+             actrl.destroy(id);
+             return id + "Deleted";
+        }
+        catch (Exception error){return id + "tidak ada";}
+    }
+    
+    @RequestMapping("/update/{id}")
+    public String updateData(@PathVariable("id") int id){
+        
+        return "data berhasil diubah";
+    }
+    
+}
